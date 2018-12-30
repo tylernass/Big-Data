430fedf95fae053e1e363c76d8898fdd56c1c26d
Tyler Nass <tyler.nass30@gmail.com>
2018-12-30 08:14:40 -0600

    Update main.py

--- Trading-Algorithms/main.py
+++ Trading-Algorithms/main.py
@@ -1,1 +1,6 @@
+import quandl, csv
+import pandas as pd
+quandl.ApiConfig.api_key = 'LK1unCQsbg3ZtttaL8gu'
 
+df1 = pd.read_csv("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=DCOILWTICO&scale=left&cosd=2013-12-27&coed=2018-12-27&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2009-06-01&line_index=1&transformation=lin&vintage_date=2018-12-30&revision_date=2018-12-30&nd=1986-01-02")
+print(df1)
