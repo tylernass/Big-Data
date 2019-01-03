02ea059aafa516f7c3940722cb1d18d3e3602e13
Tyler Nass <tyler.nass30@gmail.com>
2019-01-02 18:07:20 -0600

    Update main.py

--- Trading-Algorithms/main.py
+++ Trading-Algorithms/main.py
@@ -1,6 +1,22 @@
-import quandl, csv
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
 import pandas as pd
-quandl.ApiConfig.api_key = 'LK1unCQsbg3ZtttaL8gu'
+import pandas_datareader.data as web
 
-df1 = pd.read_csv("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=DCOILWTICO&scale=left&cosd=2013-12-27&coed=2018-12-27&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2009-06-01&line_index=1&transformation=lin&vintage_date=2018-12-30&revision_date=2018-12-30&nd=1986-01-02")
-print(df1)
+style.use('ggplot')
+df = pd.read_csv('tsla.csv', parse_dates = True, index_col=0)
+# Define the dataframe to be built out
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df.dropna(inplace=True)
+print(df.head())
+
+# Build the axes
+ax1 = plt.subplot2grid((6,1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
+plt.show()
