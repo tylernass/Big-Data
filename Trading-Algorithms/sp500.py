e9c67d5d31385ee45f291c2da20cda4ae5d69b2b
Tyler Nass <tyler.nass30@gmail.com>
2019-01-02 20:15:03 -0600

    Create sp500.py

---
+++ Trading-Algorithms/sp500.py
@@ -0,0 +1,23 @@
+Description: Code used to generate ticker symbols of S&P500 companies
+
+import bs4 as bs
+import pickle
+import requests
+
+def save_sp500_tickers():
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text)
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker)
+    
+    with open("sp500tickers.pickle", "wb") as f:
+        pickle.dump(tickers, f)
+    
+    print(tickers)
+    return ticker
+    
+
+save_sp500_tickers()
