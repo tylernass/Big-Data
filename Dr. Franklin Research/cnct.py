fa901c202a11c57e670b69c7c6aed6560de00fb8
Tyler Nass <tyler.nass30@gmail.com>
2019-01-09 18:36:34 -0600

    Update cnct.py

--- Dr. Franklin Research/cnct.py
+++ Dr. Franklin Research/cnct.py
@@ -1,3 +1,3 @@
 def cnct():
-    conn = psycopg2.connect("dbname = smd user = tylernass password = test123")
+    conn = psycopg2.connect("dbname = smd user = NA password = NA")
     mycursor = conn.cursor()
