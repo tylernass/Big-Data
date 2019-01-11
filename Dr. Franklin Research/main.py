d7e22c69a9a78488d473a0bd9dd51049e5a7d485
Tyler Nass <tyler.nass30@gmail.com>
2019-01-11 10:25:54 -0600

    Update main.py

--- Dr. Franklin Research/main.py
+++ Dr. Franklin Research/main.py
@@ -72,7 +72,7 @@ def dlt():
 
 # Step 6: Function used to create a table. Can be used anywhere.
 def tbl():
-    conn = psycopg2.connect("dbname = smd user = tylernass password = test123")
+    conn = psycopg2.connect("dbname = smd user = *** password = ***")
     sql_command = """CREATE TABLE Test(Energy DOUBLE PRECISION  NOT NULL, Zipcode INT NOT NULL, CustomerID BIGINT NOT NULL);"""
     mycursor = conn.cursor()
     mycursor.execute(sql_command)
@@ -80,8 +80,7 @@ def tbl():
     # Open csv files
     # 12/02/12018 0030
 
-# To be run in loc for every file
-# Progress: Currently need to finish running and testing scripts
+sqlval=[]
 def cvv():
     vals = []
     for i in os.listdir():
@@ -96,21 +95,22 @@ def cvv():
                 vals.append(zipcode)
                 idd = row[3]
                 vals.append(idd)
-                d = datetime.strptime(row[4], '%m/%d/%Y')
-                vals.append(d)
+                if row[4] == 'DELIVERY CLASS PASSED':
+                    continue
+                else:
+                    d = datetime.strptime(row[4], '%m/%d/%Y')
+                    vals.append(d)
                 for x in range(7, 55):
                     nrg = row[x]
                     vals.append(nrg)
-            insrt(*vals)
+        insrt(*vals)
             # Insrt(*vals) runs after every ROW in each FILE
 # Date 03/31/2015
 # Function used to connect to database
-
 # Function used to migrate data from csv file to database. To be run in relevant location.
 def insrt(*vals):
     # Vals[0:2] == Zipcode, ID, Date
     # Val[3:51] == NRG values for respective timestamps
-    sqlval = []
     timee = [0, 0, 0, 30, 1, 0, 1, 30, 2, 0, 2, 30, 3, 0, 3, 30, 4, 0, 4, 30, 5, 0, 5, 30, 6, 0, 6, 30, 7, 0, 7, 30, 8,
              0, 8, 30,
              9, 0, 9, 30, 10, 0, 10, 30, 11, 0, 11, 30, 12, 0, 12, 30, 13, 0, 13, 30, 14, 0, 14, 30, 15, 0, 15, 30, 16,
@@ -120,21 +120,21 @@ def insrt(*vals):
              30]
 
     for x in range(0, 96, 2):
-        # a = timee[x]
-        # b = timee[x + 1]
-        # t = time(a, b)
-        # date = vals[2]
-        # timestmp = datetime.combine(date, t).strftime('%m/%d/%Y %H:%M:%S')
+        a = timee[x]
+        b = timee[x + 1]
+        t = time(a, b)
+        date = vals[2]
+        timestmp = datetime.combine(date, t).strftime('%m/%d/%Y %H:%M:%S')
 
         if x == 0:
             y = 3
         else:
             y = int(x/2 + 3)
 
-        val = (vals[y], vals[0], vals[1])
+        val = (timestmp, vals[y], vals[0], vals[1])
         sqlval.append(val)
 
-    sqql = "INSERT INTO EnergyUsage(Energy, Zipcode, CustomerID) VALUES(%s);"
+    sqql = ("INSERT INTO EnergyUsage(Timestampp, Energy, Zipcode, CustomerID) VALUES (%s, %s, %s, %s)")
 
     # for x in range(0, len(sqlval)):
     #     if x == (len(sqlval)-1):
@@ -145,13 +145,9 @@ def insrt(*vals):
     #     sqql = sqql + y
     try:
         mycursor.executemany(sqql, sqlval)
-        if(mycursor.executemany(sqql, sqlval)):
-            print('Statement Executed')
-            conn.commit()
     except:
         conn.rollback()
 
-
 def cnct():
-    conn = psycopg2.connect("dbname = smd user = NA password = NA")
+    conn = psycopg2.connect("dbname = smd user = *** password = ***")
     mycursor = conn.cursor()
