31a72840a9a30f72b6ef4bf7e04f822341f613da
Tyler Nass <tyler.nass30@gmail.com>
2019-01-12 17:40:09 -0600

    Update cv.py

--- Dr. Franklin Research/cv.py
+++ Dr. Franklin Research/cv.py
@@ -1,25 +1,30 @@
 sqlval = []
+strt = 0
 def cvv():
     for i in os.listdir():
-        vals = []
         with open(i, 'r') as f:
+            vals = []
             reader = csv.reader(f, delimiter = ',')
             firstline = True
+            strt = timeit.default_timer()
             for row in reader:
                 if firstline:
                     firstline = False
                     continue
-                zipcode = row[0] # if not use .strip()
-                vals.append(zipcode)
-                idd = row[3]
-                vals.append(idd)
-                if row[4] == 'DELIVERY CLASS PASSED':
-                    continue
                 else:
-                    # d = datetime.strptime(row[4], '%m/%d/%Y')
-                    d = parse(row[4])
-                    vals.append(d)
-                for x in range(7, 55):
-                    nrg = row[x]
-                    vals.append(nrg)
+                    zipcode = row[0] # if not use .strip()
+                    vals.append(zipcode)
+                    idd = row[3]
+                    vals.append(idd)
+                    if row[4] == 'DELIVERY CLASS PASSED':
+                        continue
+                    else:
+                        # d = datetime.strptime(row[4], '%m/%d/%Y')
+                        d = row[4]
+                        vals.append(d)
+                    for x in range(7, 55):
+                        nrg = row[x]
+                        vals.append(nrg)
                 insrt(*vals)
+                print('1. Done')
+                vals.clear()
