a9158426a7b256ff1fc90afaba898af476bbdd45
Tyler Nass <tyler.nass30@gmail.com>
2019-01-12 09:38:56 -0600

    Update insrt.py

--- Dr. Franklin Research/insrt.py
+++ Dr. Franklin Research/insrt.py
@@ -1,7 +1,10 @@
+# Date 03/31/2015
+# Function used to connect to database
+# Function used to migrate data from csv file to database. To be run in relevant location.
 def insrt(*vals):
     # Vals[0:2] == Zipcode, ID, Date
     # Val[3:51] == NRG values for respective timestamps
-    sqlval = []
+
     timee = [0, 0, 0, 30, 1, 0, 1, 30, 2, 0, 2, 30, 3, 0, 3, 30, 4, 0, 4, 30, 5, 0, 5, 30, 6, 0, 6, 30, 7, 0, 7, 30, 8,
              0, 8, 30,
              9, 0, 9, 30, 10, 0, 10, 30, 11, 0, 11, 30, 12, 0, 12, 30, 13, 0, 13, 30, 14, 0, 14, 30, 15, 0, 15, 30, 16,
@@ -14,23 +17,31 @@ def insrt(*vals):
         a = timee[x]
         b = timee[x + 1]
         t = time(a, b)
-        # date = datetime.strptime(vals[2], "%m/%d/%Y")
-        timestmp = datetime.combine(vals[2], t)
+        date = vals[2]
+        try:
+            timestmp = datetime.combine(date, t).strftime('%Y-%m-%d %H:%M:%S')
+        except:
+            continue
+
         if x == 0:
-            y = 0
+            y = 3
         else:
-            y = x/2 + 3
-        val = [(timestmp), vals[y], vals[0], vals[1]]
+            y = int(x/2 + 3)
+
+        val = (timestmp, vals[y], vals[0], vals[1])
         sqlval.append(val)
 
+    sqql = ("INSERT INTO EnergyUsage(Timestampp, Energy, Zipcode, CustomerID) VALUES (%s, %s, %s, %s)")
 
-    sqql = "INSERT INTO smd(Timestampp, Energy, Zipcode, CustomerID) VALUES "
-    for x in range(0, 48):
-        if x == 47:
-            y = "(%s, %s, %s, %s);"
-        else:
-            y = "(%s, %s, %s, %s), "
-
-        sqql = sqql + y
-
-    mycursor.execute(sqql, sqlval)
+    # for x in range(0, len(sqlval)):
+    #     if x == (len(sqlval)-1):
+    #         y = "(%s, %s, %s, %s)"
+    #     else:
+    #         y = "(%s, %s, %s, %s), "
+    #
+    #     sqql = sqql + y
+    try:
+        mycursor.executemany(sqql, sqlval)
+        sqlval.clear()
+    except:
+        conn.rollback()
